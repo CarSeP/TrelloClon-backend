5f7da675e1c478eb8faf3d0090f2cbe34ef77262
CarSeP <vjpd64@gmail.com>
2025-05-24 09:40:10 -0400

    Use cors

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { boardRouter } from "./src/routes/board.route";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/board", boardRouter);
 
 app.listen(port, () => {
