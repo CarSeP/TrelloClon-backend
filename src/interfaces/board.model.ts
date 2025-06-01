b4f2376ac26e29f58ea1a7ba26bf6bea377d3e00
CarSeP <vjpd64@gmail.com>
2025-06-01 17:43:22 -0400

    Update board model

--- src/interfaces/board.model.ts
+++ src/interfaces/board.model.ts
@@ -1,5 +1,35 @@
 export interface BoardType {
 	id: string;
 	title: string;
-	columns: any[];
+	usersID: string[];
+	columns?: ColumnType[];
+	createdAt?: Date;
+	updatedAt?: Date;
+}
+
+export interface ColumnType {
+	id: string;
+	title: string;
+	cards: CardType[];
+}
+
+export interface CardType {
+	id: string;
+	title: string;
+	description: string;
+	createdAt: {
+		date: Date;
+		userID: string;
+	};
+	updatedAt: [
+		{
+			userID: string;
+			date: Date;
+			change: "description" | "title" | "status" | "column";
+			value: {
+				oldValue: string;
+				newValue: string;
+			};
+		},
+	];
 }
